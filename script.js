e9377b172a52ae23fafea38e42d1069fbcfdd3ee
PopoolaHakeem <hakeempopoola04@gmail.com>
2025-09-22 02:53:08 +0100

    first commit

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 // alert('hello')
+const btn = document.querySelector('.dropdownDefaultButton');
+    const dropDownContent = document.getElementById('dropdown');
+    
+    btn.addEventListener('click', function() {
+        dropDownContent.classList.toggle('hidden');
+    });
