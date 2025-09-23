a08a05b89149387646b6f986a9c8f2cc4ac910ea
PopoolaHakeem <hakeempopoola04@gmail.com>
2025-09-23 16:00:58 +0100

    first commit

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
 // alert('hello')
-const btn = document.querySelector('.dropdownDefaultButton');
-    const dropDownContent = document.getElementById('dropdown');
-    
-    btn.addEventListener('click', function() {
-        dropDownContent.classList.toggle('hidden');
-    });
+const btn = document.querySelector('.dropdownDefaultButtonn');
+const dropDownContent = document.getElementById('dropdownn');
+
+
+btn.addEventListener('click', function () {
+    dropDownContent.classList.toggle('hidden');
+});
+
+const serviceBtn = document.querySelector('.serviceDropdownButton');
+const serviceContent = document.getElementById('serviceBtn');
+
+serviceBtn.addEventListener('click', function () {
+    serviceContent.classList.toggle('hidden');
+});
+
+// const navBarBg = document.getElementById('navBar');
+
+// window.addEventListener('scroll', () => {
+//     if (window.scrollY > 1) {
+//         navBarBg.style.backgroundColor = '#333'
+//         navBarBg.style.boxShadow = '0 2px 4px rgba(0, 0, 0, 0.3)'
+//         alert('hi')
+//     } 
+//     else {
+//         navBarBg.style.backgroundColor = 'transparent'
+//         navBarBg.style.boxShadow = 'none'
+//     }
+
+// });
+
+// function () {
+//     document.getElementById('navBar').style.scrollbarColor = 'red blue'
+// }
